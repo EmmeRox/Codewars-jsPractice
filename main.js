c40a4299aba55f69e894cdf5a600511aa194df0f
Emmeline <99152085+EmmeRox@users.noreply.github.com>
2023-05-25 21:07:31 -0500

    added new codewars, a 6 kyu

--- main.js
+++ main.js
@@ -331,4 +331,24 @@ function spacey(array) {
   return newArr;
 }
 
-console.log(spacey(["kevin", "has", "no", "space"]));
+//console.log(spacey(["kevin", "has", "no", "space"]));
+
+//implement the function which takes an array containing the names of people that like an iteme
+
+function likes(names) {
+  if (names.length === 0) {
+    return `no one likes this`
+  } else if (names.length === 1) {
+    return `${names} likes this`
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  } else {
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+  }
+  console.log(names.length)
+}
+
+console.log(likes(['Jacob', 'Alex', 'Joe', 'Mary', 'Tammy']));
+console.log(likes([]))
