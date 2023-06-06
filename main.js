01ddba8fe5ec7af733f90074566d5a0e12d8c36f
Emmeline <99152085+EmmeRox@users.noreply.github.com>
2023-06-06 16:50:10 -0500

    added first challenge from leetcode

--- main.js
+++ main.js
@@ -444,3 +444,13 @@ function solve(arr) {
 }
 
 //console.log(solve([3, 4, 4, 3, 6, 3]));
+
+Array.prototype.last = function() {
+  if (this.length === 0) {
+    return -1;
+} else {
+return this[this.length - 1] }
+}
+
+const arr = [1, 2, 3, 4]
+console.log(arr.last());
