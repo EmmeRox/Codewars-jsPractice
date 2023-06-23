929d42e48589b4ea92c9ef115887178689825bae
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-06-23 14:41:44 -0500

    merging issues

--- main.js
+++ main.js
@@ -608,29 +608,4 @@ function binomialCoefficient(numElements, numElementsChosen) {
       nFactorial(numElements - numElementsChosen));
 
   return numCombinations;
-=======
-function factorial(num) {
- for (let i = 1; i <= num; i++) {
- num *= num;
-return num;
->>>>>>> 36c5d6f6f68ff0a66d85df8634e8767ae61ba14e
-}
-}
 ; //separate the bottom function into two functions. first one being the one that does the math of factorial and then calling the function inside the checkchoose function, using it in lieu of the (!) from the original problem
-
-function checkchoose(m, n) { 
-  let colorCombo = 1;
-  
-  for (let i = 0; i <= m; i++) {
-    colorCombo = factorial(n) / (factorial (i) * factorial(n - i);
-  }
-
-  if (m === colorCombo) {
-    return ;
-  }
-
-}
-
-return -1;
-}
-console.log(checkchoose(6, 4));
